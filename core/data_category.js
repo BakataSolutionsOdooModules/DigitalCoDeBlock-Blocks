0fd27254623340152dc1ba9a2db7e28360c3143c
Joel Hernandez Fariña <joel.informatica@academiamaker.es>
2024-10-09 15:21:05 +0100

    Añadido bloques añadir a compressed

--- core/data_category.js
+++ core/data_category.js
@@ -153,6 +153,20 @@ Blockly.DataCategory.addChangeVariableBy = function(xmlList, variable) {
       'VARIABLE', ['VALUE', 'math_number', 1]);
 };
 
+
+Blockly.DataCategory.addDataCreateListOfItems = function(xmlList, variable) {
+  // <block type="data_create_list_with">
+  //   <field name="LIST">variablename</field>
+  //   <value name="ITEMS">
+  //     <shadow type="data_listcontents">
+  //       <field name="LIST">variablename</field>
+  //     </shadow>
+  //   </value>
+  // </block>
+  Blockly.DataCategory.addBlock(xmlList, variable, 'data_createListOfItems', 'LIST');
+}
+
+
 /**
  * Construct and add a data_showVariable block to xmlList.
  * @param {!Array.<!Element>} xmlList Array of XML block elements.
@@ -214,18 +228,6 @@ Blockly.DataCategory.addAddToList = function(xmlList, variable) {
   Blockly.DataCategory.addBlock(xmlList, variable, 'data_addtolist', 'LIST',
       ['ITEM', 'text', Blockly.Msg.DEFAULT_LIST_ITEM]);
 };
-
-Blockly.DataCategory.addDataCreateListOfItems = function(xmlList, variable) {
-  // <block type="data_create_list_with">
-  //   <field name="LIST">variablename</field>
-  //   <value name="ITEMS">
-  //     <shadow type="data_listcontents">
-  //       <field name="LIST">variablename</field>
-  //     </shadow>
-  //   </value>
-  // </block>
-  Blockly.DataCategory.addBlock(xmlList, variable, 'data_createListOfItems', 'LIST');
-}
 
 /**
  * Construct and add a data_deleteoflist block to xmlList.
